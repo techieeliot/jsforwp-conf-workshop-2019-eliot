fc48f83cd878c2483b7e623ac92536e7cfad71dd
Eliot Sanford <eliot587@gmail.com>
2019-07-12 16:58:22 -0500

    added additional additional node modules for react folder

---
+++ react/starter/build/precache-manifest.154331873f859c02b4eecbeb728347ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362ed537e2cdd8f0c7d1511d7b330da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f324c4fd46372bf60c6a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f324c4fd46372bf60c6a",
+    "url": "/static/js/main.df0391fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ react/step-00-practice/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+
+function App() {
+
+  function getUserName(){
+    
+    const userName
+  }
+
+  return (
+    <div className="App">
+      <button onClick={$}>UserName Here</button>
+    </div>
+  );
+
+}
+
+
+
+export default App;
